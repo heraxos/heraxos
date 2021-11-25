d793d1fd2b3754622036aee0dea41bcefffb1f45
Armen <91148484+heraxos@users.noreply.github.com>
2021-11-25 22:48:30 +0300

    Create js7.js

---
+++ js7.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.slick-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		speed: 2000
+	})
+})
